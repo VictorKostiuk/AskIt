4fbb649a18a19577a3ff8e4fc1fc9c2d50892207
Viktor Kostiuk <VictorKostiuk>
2022-03-07 22:44:11 +0200

    Added tags

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,10 +6,19 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
   belongs_to :user
 
+  has_many :question_tags, dependent: :destroy
+  has_many :tags, through: :question_tags
+
   validates :title, presence: true, length: { minimum: 2 }
   validates :body, presence: true, length: { minimum: 2 }
 
   def formatted_created_at
     created_at.strftime('%Y-%m-%d %H:%M:%S')
+  end
+
+  scope :all_by_tags, ->(tag_ids) do
+    questions = includes(:user, :question_tags, :tags)
+    questions = questions.joins(:tags).where(tags: tag_ids) if tag_ids
+    questions.order(created_at: :desc)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
-# 30.times do
-#   title = Faker::Hipster.sentence(word_count: 3)
-#   body = Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 4)
-#   Question.create title: title, body: body
-# end
+10.times do
+  title = Faker::Hipster.sentence(word_count: 3)
+  body = Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 4)
+  Question.create title: title, body: body
+end
 
-User.find_each { |u| u.send(:set_gravatar_hash) ; u.save}
+# User.find_each { |u| u.send(:set_gravatar_hash) ; u.save}
+
+
+10.times do
+  title = Faker::Hipster.word
+  Tag.create title: title
+end
